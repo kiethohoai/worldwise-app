fa0e545b5f83d03683f2c439c02792f70065ced2
kiethohoai <kietho.060193@gmail.com>
2024-07-08 22:48:18 +0700

    014 Dynamic Routes With URL Parameters

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 
 const formatDate = (date) =>
@@ -8,15 +9,15 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
 
   return (
-    <div className={styles.cityItem}>
+    <Link className={styles.cityItem} to={`${id}`}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>{formatDate(date)}</time>
       <button className={styles.deleteBtn}>&times;</button>
-    </div>
+    </Link>
   );
 }
 
